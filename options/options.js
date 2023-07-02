fcd5ae587065ac17520ebc96f94394819ab15b8a
Crowdbotics <team@crowdbotics.com>
2023-07-02 17:11:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-paper-41860",
+  url: "https://gentle-paper-41860.botics.co",
+  api: "https://gentle-paper-41860.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
